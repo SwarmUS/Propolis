e74a43d8a99248b7cf0000fdbd9ba1eaeb670a8a
xgroleau <xavgroleau@gmail.com>
2021-01-31 10:51:34 -0500

    Added missing doc

--- src/pheromones/hivemind-host/include/hivemind-host/IHiveMindHostDeserializer.h
+++ src/pheromones/hivemind-host/include/hivemind-host/IHiveMindHostDeserializer.h
@@ -12,6 +12,10 @@ class IHiveMindHostDeserializer {
   public:
     virtual ~IHiveMindHostDeserializer() = default;
 
+    /**
+     *@brief Deserializes a DTO message from a data stream
+     *
+     *@return true if the operation was successful, false if not*/
     virtual std::variant<std::monostate, MessageDTO> deserializeFromStream() = 0;
 };
 
