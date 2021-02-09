57b0bb18162af91fd3f914c2795995305c2f21db
Xavier Groleau <xavgroleau@gmail.com>
2021-02-09 15:08:35 -0500

    bump pheromone

--- cmake/pheromones/common.cmake
+++ cmake/pheromones/common.cmake
@@ -13,7 +13,7 @@ function(pheromones_fetch_populate)
         ${PROJECT_NAME}_pheromones
 
         GIT_REPOSITORY https://github.com/SwarmUS/Pheromones
-        GIT_TAG        feat/SWARINFO-196_adjust-fields
+        GIT_TAG        616cc62
         GIT_PROGRESS   TRUE
     )
 
