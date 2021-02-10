3b8c60b3385c337134c4b8cec1d96a7ad29a184a
Xavier Groleau <xavgroleau@gmail.com>
2021-02-10 09:12:25 -0500

    wip doc

--- src/cpp-common/include/cpp-common/ICircularQueue.h
+++ src/cpp-common/include/cpp-common/ICircularQueue.h
@@ -4,6 +4,9 @@
 #include <cstdint>
 #include <optional>
 
+/**
+ *@brief A queue that add and pop items circularly, similar to a circular buffer
+ **/
 template <typename T>
 class ICircularQueue {
   public:
@@ -11,10 +14,25 @@ class ICircularQueue {
 
     virtual ~ICircularQueue<T>() = default;
 
+    /**
+     *@brief push an item into the queue
+     *
+     *@return true if the operation was successfull, false if not (i.e. buffer is full)
+     **/
     virtual bool push(T item) = 0;
 
+    /**
+     *@brief get an item from queue and removes it. It gets then pop de data.
+     *
+     *@return the item or nothing if the queue was empty
+     **/
     virtual std::optional<T> get() = 0;
 
+    /**
+     *@brief peek at the next data
+     *
+     *@return the next item to get
+     **/
     virtual const std::optional<std::reference_wrapper<const T>> peek() const = 0;
 
     virtual void pop() = 0;
