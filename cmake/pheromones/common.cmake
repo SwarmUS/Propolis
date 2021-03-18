072f713eabf93b08480b038b43f98c82f25d3fad
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-03-18 17:15:55 -0400

    Added comment to remind to change commit tag

--- cmake/pheromones/common.cmake
+++ cmake/pheromones/common.cmake
@@ -13,7 +13,7 @@ function(pheromones_fetch_populate)
         ${PROJECT_NAME}_pheromones
 
         GIT_REPOSITORY https://github.com/SwarmUS/Pheromones
-        GIT_TAG         5032bde
+        GIT_TAG         5032bde # Change for commit on master when merged
         GIT_PROGRESS   TRUE
     )
 
