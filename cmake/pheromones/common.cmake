f78800cf28d3e9f584ca0b84ac38209bfec10112
Gabriel Lessard <lesg2605@usherbrooke.ca>
2021-03-23 18:50:37 -0400

    Bumped pheromones

--- cmake/pheromones/common.cmake
+++ cmake/pheromones/common.cmake
@@ -13,7 +13,7 @@ function(pheromones_fetch_populate)
         ${PROJECT_NAME}_pheromones
 
         GIT_REPOSITORY https://github.com/SwarmUS/Pheromones
-        GIT_TAG         c629de5  # Change for commit on master when merged
+        GIT_TAG         462b9a8  # Change for commit on master when merged
         GIT_PROGRESS   TRUE
     )
 
