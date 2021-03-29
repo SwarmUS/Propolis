7639faf4a1f96019ac5913ff997da2224f1db03a
Hornet <50376785+g-lesssard@users.noreply.github.com>
2021-03-29 13:03:00 -0400

    Fix/abstract task thread (#29)

--- src/os/posix/include/OSMacros.h
+++ src/os/posix/include/OSMacros.h
@@ -2,9 +2,10 @@
 #define __MACROS_H__
 
 #include <stdint.h>
+#include <thread>
 
 typedef unsigned long UBaseType_t;
-typedef uint8_t TaskHandle_t;
+typedef std::thread& TaskHandle_t;
 typedef void (*TaskFunction_t)(void*);
 
 #define configMINIMAL_STACK_SIZE 1
