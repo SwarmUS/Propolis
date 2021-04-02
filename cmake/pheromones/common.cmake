092784236b06568ade8db3eaa40501304ed74d97
xgroleau <xavgroleau@gmail.com>
2021-04-02 16:12:14 -0400

    bump pheromones

--- cmake/pheromones/common.cmake
+++ cmake/pheromones/common.cmake
@@ -13,7 +13,7 @@ function(pheromones_fetch_populate)
         ${PROJECT_NAME}_pheromones
 
         GIT_REPOSITORY https://github.com/SwarmUS/Pheromones
-        GIT_TAG        cda3802
+        GIT_TAG        ad00e7f
         GIT_PROGRESS   TRUE
     )
 
