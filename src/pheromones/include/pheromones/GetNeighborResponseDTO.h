3d41894ee69526384b023f16fabdae1455f20bba
Xavier Groleau <xavgroleau@gmail.com>
2021-04-08 16:35:37 -0400

    made response optional

--- src/pheromones/include/pheromones/GetNeighborResponseDTO.h
+++ src/pheromones/include/pheromones/GetNeighborResponseDTO.h
@@ -3,11 +3,12 @@
 
 #include "NeighborPositionDTO.h"
 #include <message.pb.h>
+#include <optional>
 
 class GetNeighborResponseDTO {
   public:
     GetNeighborResponseDTO(const GetNeighborResponse resp);
-    GetNeighborResponseDTO(uint32_t neighborId, const NeighborPositionDTO& pos);
+    GetNeighborResponseDTO(uint32_t neighborId, const std::optional<NeighborPositionDTO>& pos);
 
     /**@brief get the id of the neighbor
      *@return the id of the neighbor */
@@ -15,7 +16,7 @@ class GetNeighborResponseDTO {
 
     /**@brief get the relative position
      *@return the relative position*/
-    const NeighborPositionDTO& getNeighborPosition() const;
+    const std::optional<NeighborPositionDTO>& getNeighborPosition() const;
 
     /**@brief set the id of the neighbor
      *@param id the id of the neighbor to set*/
@@ -23,7 +24,7 @@ class GetNeighborResponseDTO {
 
     /**@brief set the position of the neighbor
      *@param pos the new position of the neighbor*/
-    void setNeighborPosition(const NeighborPositionDTO& pos);
+    void setNeighborPosition(const std::optional<NeighborPositionDTO>& pos);
 
     /**
      *@brief serialize a GetNeighborResponse for nanopb, sets the fields properly before using
@@ -34,7 +35,7 @@ class GetNeighborResponseDTO {
 
   private:
     uint32_t m_neighborId;
-    NeighborPositionDTO m_pos;
+    std::optional<NeighborPositionDTO> m_pos;
 };
 
 #endif // __GETNEIGHBORRESPONSEDTO_H_
--- src/pheromones/src/GetNeighborResponseDTO.cpp
+++ src/pheromones/src/GetNeighborResponseDTO.cpp
@@ -1,22 +1,35 @@
 #include "GetNeighborResponseDTO.h"
 
 GetNeighborResponseDTO::GetNeighborResponseDTO(const GetNeighborResponse resp) :
-    m_neighborId(resp.neighbor_id), m_pos(resp.position) {}
+    m_neighborId(resp.neighbor_id) {
+    if (resp.has_position) {
+        m_pos = NeighborPositionDTO(resp.position);
+    } else {
+        m_pos = {};
+    }
+}
 
 GetNeighborResponseDTO::GetNeighborResponseDTO(uint32_t neighborId,
-                                               const NeighborPositionDTO& pos) :
+                                               const std::optional<NeighborPositionDTO>& pos) :
     m_neighborId(neighborId), m_pos(pos) {}
 
 uint32_t GetNeighborResponseDTO::getNeighborId() const { return m_neighborId; }
 
-const NeighborPositionDTO& GetNeighborResponseDTO::getNeighborPosition() const { return m_pos; }
+const std::optional<NeighborPositionDTO>& GetNeighborResponseDTO::getNeighborPosition() const {
+    return m_pos;
+}
 
 void GetNeighborResponseDTO::setNeighborId(uint32_t id) { m_neighborId = id; }
 
-void GetNeighborResponseDTO::setNeighborPosition(const NeighborPositionDTO& pos) { m_pos = pos; }
+void GetNeighborResponseDTO::setNeighborPosition(const std::optional<NeighborPositionDTO>& pos) {
+    m_pos = pos;
+}
 
 bool GetNeighborResponseDTO::serialize(GetNeighborResponse& resp) const {
     resp.neighbor_id = m_neighborId;
-    resp.has_position = true;
-    return m_pos.serialize(resp.position);
+    resp.has_position = m_pos.has_value();
+    if (m_pos) {
+        return m_pos.value().serialize(resp.position);
+    }
+    return true;
 }
