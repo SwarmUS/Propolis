4761fd3f668857e06f155b0a07d80984fa9bc3c8
Xavier Groleau <xavgroleau@gmail.com>
2021-09-23 08:22:40 -0400

    Fix/swarinfo 547 frequency not matching delay (#56)
    * Added delay until
    
    * Fix
    
    * fix
    
    * fix
    
    * fix
    
    * removed useless ;

--- src/os/freertos/include/Task.h
+++ src/os/freertos/include/Task.h
@@ -6,9 +6,17 @@
 #include <stdint.h>
 
 namespace Task {
-    inline void delay(TickType_t ms) { vTaskDelay(ms); }
+
+    typedef TickType_t Time;
+
+    inline void delay(uint32_t ms) { vTaskDelay(ms); }
+
+    inline void delayUntil(Time& current, uint32_t ms) { vTaskDelayUntil(&current, ms); }
+
+    inline Time getTime() { return xTaskGetTickCount(); }
 
     inline void startScheduler() { vTaskStartScheduler(); }
+
 } // namespace Task
 
 #endif //__TASK_H__
--- src/os/posix/include/Task.h
+++ src/os/posix/include/Task.h
@@ -1,13 +1,25 @@
 #ifndef __TASK_H__
 #define __TASK_H__
 
+#include <chrono>
 #include <errno.h>
 #include <stdint.h>
 #include <thread>
 #include <time.h>
 
 namespace Task {
-    inline void delay(int ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
+
+    typedef std::chrono::time_point<std::chrono::system_clock> Time;
+
+    inline void delay(uint ms) { std::this_thread::sleep_for(std::chrono::milliseconds(ms)); }
+
+    inline void delayUntil(Time& current, uint ms) {
+
+        std::this_thread::sleep_until(current + std::chrono::milliseconds(ms));
+        current = std::chrono::system_clock::now();
+    }
+
+    inline Time getTime() { return std::chrono::system_clock::now(); }
 
     inline void startScheduler() {
         while (true) {
